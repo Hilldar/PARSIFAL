8aa3a11212e1a9e48981307b66b5a971ae5a6429
Hilldar <riccardo@srv-lab.fe.infn.it>
2021-04-19 15:40:35 +0200

    First version on GIT

--- init_PARSIFAL.sh
+++ init_PARSIFAL.sh
@@ -6,7 +6,7 @@ echo "export PARSIFAL2=$PARSIFAL2_home">>$bash_name
 echo "export parsifal2=$PARSIFAL2">>$bash_name
 echo "alias  PARSIFAL2='$PARSIFAL2_home/PARSIFAL2.sh'">>$bash_name
 echo "alias  parsifal2='PARSIFAL2'">>$bash_name
-echo "export PARSIFAL2_data=$PWD">>$bash_name
+echo "export PARSIFAL2_data=$PWD/data">>$bash_name
 echo "export exe_parsifal2=$PWD/PARSIFAL2.sh">>$bash_name
 echo "function cd_parsifal(){">>$bash_name
 echo "cd $PARSIFAL2;">>$bash_name
